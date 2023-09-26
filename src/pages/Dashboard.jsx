979cfcd0df3c6324920ed42cfea8844d60854633
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-09-26 09:56:58 -0600

    playing with styled props in chakra, laning how to used normal css properties not availble as props in chakra on my components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,34 @@
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
+
+  const boxStyles = {
+    p: "10",
+    bg: "purple.400",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    filter: "blur(2px)",
+    ':hover': {
+      color: "black",
+      bg: "blue.200"
+    }
+  }
+
   return (
-    <div>Dashboard</div>
+    <Container as="section" maxWidth="4xl">
+      <Heading my="30px" p="10px">Hello</Heading>
+      <Text marginLeft="30px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, ratione?</Text>
+      <Text ml="30px" color="blue.300" fontWeight="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, ratione?</Text>
+
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is a Box</Text>
+      </Box>
+
+      <Box sx={boxStyles}>
+        Hello, Ninjas
+      </Box>
+
+    </Container>
   )
 }
