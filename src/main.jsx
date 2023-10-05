adadf417787ca61933397419a94fce90fe3159b7
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-10-05 07:20:38 -0600

    adding toast and avatars

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme()
+
+//extend theme
+const colors= {
+  brand:{
+    
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
